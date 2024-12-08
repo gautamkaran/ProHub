41267a0ec4bb4b9446a66bfed3d72aa3db47deb7
gautamkaran <karankumargautam.dev@gmail.com>
2024-12-08 18:20:55 +0530

    fix my router config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div className="bg-red-50">Home</div>;
+  return (
+    <div className="bg-red-50">
+      <Link to="/">Home</Link>
+      <Link to="/project">project</Link>
+      <Link to="/about">about</Link>
+    </div>
+  );
 };
 
 export default Home;
